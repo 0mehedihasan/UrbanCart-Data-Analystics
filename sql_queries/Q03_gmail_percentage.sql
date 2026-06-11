32769d2623a6bc3f8d05a00b26d70c9be5edba31
Rubina Akter <nishichowdhury2419@gmail.com>
2026-06-12 01:41:18 +0600

    Update Q03_gmail_percentage.sql

--- sql_queries/Q03_gmail_percentage.sql
+++ sql_queries/Q03_gmail_percentage.sql
@@ -1,1 +1,13 @@
+-- Question 3: What percent of total customers uses gmail?
+
+select
+round(
+count(
+case
+when "email" ilike('%gmail%')
+then "customer_id"
+end
+) * 100.0 / count(*)
+,2) as "Gmail Percentage"
+from public."DimCustomers"
 
