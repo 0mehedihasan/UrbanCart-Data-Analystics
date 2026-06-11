2e073591c6ca53e20484c9bb3fbda936afa7b149
Rubina Akter <nishichowdhury2419@gmail.com>
2026-06-12 01:40:40 +0600

    Update Q02_city_orders_revenue.sql

--- sql_queries/Q02_city_orders_revenue.sql
+++ sql_queries/Q02_city_orders_revenue.sql
@@ -1,1 +1,17 @@
+__ Question 2: Which cities generate the highest 
+number of orders & revenue?
 
+
+select
+c."city",
+count(distinct o."order_id") as "Total Orders",
+sum(oi."quantity" * p."unit_price") as "Total Revenue"
+from public."FactOrders" o
+left join public."DimCustomers" c
+on o."customer_id" = c."customer_id"
+left join public."FactOrderItems" oi
+on o."order_id" = oi."order_id"
+left join public."DimProducts" p
+on oi."product_id" = p."product_id"
+group by 1
+order by 3 desc;
